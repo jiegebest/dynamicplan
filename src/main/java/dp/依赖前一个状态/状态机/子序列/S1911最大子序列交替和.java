22215b3ce7e704c84b8fad8a202ebc7cdb251f71
jiewang04 <jiewang04@yangqianguan.com>
2022-06-16 02:22:59 +0800

    .idea/

--- src/main/java/dp/依赖前一个状态/状态机/子序列/S1911最大子序列交替和.java
+++ src/main/java/dp/依赖前一个状态/状态机/子序列/S1911最大子序列交替和.java
@@ -2,9 +2,11 @@ package dp.依赖前一个状态.状态机.子序列;
 
 /**
  * 状态机
- *
  * odd[i]   奇数下标结尾最大和
  * even[i] 偶数下标结尾最大和
+ * f[i] 代表以当前元素结尾的子序列最大和
+ * 1 <= nums[i] <= 105 数字都是正数，所以直接取 even[n - 1] 就可以
+ * 注意数据类型是 long
  */
 public class S1911最大子序列交替和 {
 
@@ -14,14 +16,10 @@ public class S1911最大子序列交替和 {
 		long[] even = new long[n];
 		odd[0] = 0;
 		even[0] = nums[0];
-
 		for (int i = 1; i < n; ++i) {
-			// 最后一个选择的下标是奇数，要-
 			odd[i] = Math.max(odd[i - 1], even[i - 1] - nums[i]);
-			// 最后一个选择的下标是偶数，要+
 			even[i] = Math.max(even[i - 1], odd[i - 1] + nums[i]);
 		}
-
 		return even[n - 1];
 	}
 
